ddcb1dbed78a7665d1c6428609e48638631b305e
simpletrain <zsy498761554@live.com>
2015-09-10 15:47:38 +0800

    delete some require in calculate-spec

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var Calculator = require('../src/calculator.js');
-var Book = require('../src/book.js');
-var Cart = require('../src/cart.js');
 
 describe('Calculator', function () {
 
@@ -13,17 +11,13 @@ describe('Calculator', function () {
     describe('#calculateAmount', function () {
 
         it('can calculate right amount', function () {
-            var book1 = new Book(1);
-            var book2 = new Book(2);
-            var book3 = new Book(3);
-            var book4 = new Book(4);
-            var book5 = new Book(5);
-            var cart = new Cart();
-            cart.addBooks(book1, 1);
-            cart.addBooks(book2, 1);
-            cart.addBooks(book3, 2);
-            cart.addBooks(book4, 2);
-            cart.addBooks(book5, 2);
+            var cart = {
+                bookOneCount : 1,
+                bookTwoCount : 2,
+                bookThreeCount : 1,
+                bookFourCount : 2,
+                bookFiveCount : 2
+            };
             var calculator = new Calculator();
             var amount = calculator.calculateAmount(cart);
             expect(amount).toBe(51.2);
